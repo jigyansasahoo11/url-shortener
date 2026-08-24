68f0864b5f7fc55583edfd97d458331cb1be52c1
jigyansasahoo11 <jigyansasahoo105@gmail.com>
2026-08-24 21:44:02 +0530

    Fix duplicate setup in vite.config.js

--- url-shortener-frontend/vite.config.js
+++ url-shortener-frontend/vite.config.js
@@ -1,11 +1,14 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'   // ← Yeh line important hai
+import tailwindcss from '@tailwindcss/vite'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss()
   ],
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json']
+  }
 })
